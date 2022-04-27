50a339b7b8f4751187ec2232c232f1327f1d0838
Anabela <anabela.tot@vivifyideas.com>
2022-04-27 07:53:00 +0200

    project initialize

--- todo_project/todo_app/models.py
+++ todo_project/todo_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    URGENT_PRIORITY = 'U'
+    HIGH_PRIORITY = 'H'
+    MEDIUM_PRIORITY = 'M'
+    LOW_PRIORITY = 'L'
+
+    TASK_PRIORITY_CHOICES = [
+        (URGENT_PRIORITY, 'Urgent'),
+        (HIGH_PRIORITY, 'High'),
+        (MEDIUM_PRIORITY, 'Medium'),
+        (LOW_PRIORITY, 'Low')
+    ]
+    task_title = models.CharField(max_length=100)
+    task_priority = models.CharField(
+        max_length=1,
+        choices=TASK_PRIORITY_CHOICES,
+        default=MEDIUM_PRIORITY,
+    )
