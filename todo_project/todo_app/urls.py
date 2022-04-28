154c226a8d2e4d985739afebbebfe6a096718e1c
Anabela <anabela.tot@vivifyideas.com>
2022-04-28 15:57:54 +0200

    gg

--- todo_project/todo_app/urls.py
+++ todo_project/todo_app/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name="home"),
+<<<<<<< Updated upstream
     path('users/', views.UserAPIView.as_view())
 
+=======
+    path('users/', views.UserViewSet.as_view({'get': 'retrieve'})),
+    path('users/<int:id>', views.dynamic_lookup_view, name="user")
+>>>>>>> Stashed changes
 ]
--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -5,6 +5,25 @@ from .serializers import UserSerializer
 from todo_app.models import User
 
 
+<<<<<<< Updated upstream
+=======
+def dynamic_lookup_view(request, id):
+    obj = User.objects.get(id=1)
+    context = {
+        "object": obj
+    }
+    return(request, "user.html", context)
+
+
+class UserViewSet(viewsets.ViewSet):
+
+    def retrieve(self, request, *args, **kwargs):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+
+>>>>>>> Stashed changes
 class HomeView(APIView):
 
     def get(self, request, *args, **kwargs):
