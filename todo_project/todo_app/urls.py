fb74a293cbd99f32f31d0db12dcc2411c2cd5a80
Anabela <anabela.tot@vivifyideas.com>
2022-04-29 08:19:05 +0200

    jwt setup

--- todo_project/todo_app/urls.py
+++ todo_project/todo_app/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name="home"),
     path('users/', views.UserViewSet.as_view({'get': 'retrieve'})),
     path('users/<int:my_id>/', views.IdUserAPIView.as_view(), name="user"),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
